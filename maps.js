12d65bae4549caf37813ebed5edaf1722517b9da
hemanth.hm <hemanth.hm@gmail.com>
2014-03-17 21:59:21 +0530

    Basic map

---
+++ maps.js
@@ -0,0 +1,9 @@
+let m = new Map();
+m.set('answer', 42);
+m.get('answeer');                // 42
+m.has('answer');                //  true
+m.delete('answer');               //  true
+m.has('answer');                  //  false
+
+m.set(keyFunc,function() {return "foo";});
+m.get(keyFunc)(); // "foo"
