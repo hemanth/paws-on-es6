58e1bf1046af4897bf414a91dc5a2c5de6db20ed
hemanth.hm <hemanth.hm@gmail.com>
2014-03-19 21:38:24 +0530

    Enhanced object literals

--- object-literal.js
+++ object-literal.js
@@ -1,5 +1,10 @@
 var greet = {
+  __proto__: theProtoObj,
+  handler, // Instead of handler: handler
   world() {
     return "Hello World!";
+  },
+  toString() {
+    return "Results: " + super.toString()
   }
 };
