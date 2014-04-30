673ec0083c4c67a56e545728b38c99b8b7123dcf
Michael Hatch <michaelhatch@me.com>
2014-04-29 21:15:45 -0700

    'answeer' -> 'answer'

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 let m = new Map();
 m.set('answer', 42);
-m.get('answeer');                // 42
+m.get('answer');                // 42
 m.has('answer');                //  true
 m.delete('answer');               //  true
 m.has('answer');                  //  false
