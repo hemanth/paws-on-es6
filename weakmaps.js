d30e40b96981063d4a3c9ab56b22bc611e4825aa
Hemanth.HM <hemanth.hm@gmail.com>
2014-05-23 19:17:08 +0530

    Added weakmap

---
+++ weakmaps.js
@@ -0,0 +1,17 @@
+var wm = new WeakMap();
+
+wm.set('life'); //TypeError: Invalid value used as weak map key
+
+wm.set('life', 'life'.length) //TypeError: Invalid value used as weak map key
+
+var wmk = {};
+
+wm.set(wmk,'life');
+
+wm.get(wmk); // "life"
+
+wm.has(wmk); // true
+
+wm.delete(wmk); // true
+
+wm.has(wmk); //false
