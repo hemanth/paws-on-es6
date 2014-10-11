84c955d254a568ce66d5d652ff988318306369d3
hemanth.hm <hemanth.hm@gmail.com>
2014-10-11 18:30:11 +0530

    Property method assignment.

---
+++ property-method-assignment.js
@@ -0,0 +1,17 @@
+ler person = {
+  get name() {
+    return this._name;
+  },
+  set name(val){
+    console.log("Setting name: " + val);
+    this._name = val;
+  }
+};
+
+/*
+> person.name = "Hemanth"
+"Hemanth"
+"Setting name: Hemanth"
+
+> person.name
+"Hemanth"
