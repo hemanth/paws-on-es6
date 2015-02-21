844241b7224ee05afe49dd380c06572b40917979
hemanth.hm <hemanth.hm@gmail.com>
2015-02-21 10:41:09 +0530

    Simple example for lexical scope.
    No more `that=this`

--- arrow.js
+++ arrow.js
@@ -1,3 +1,10 @@
 fact = (n) => { return n === 0 ? 1 : n * fact(n-1); } 
 
 fib = (n) => { return n < 2 ? n : fib(n-1) + fib(n-2); }
+
+let Person = (fname, lname, age) => {
+   this.fname= fname;
+   this.lname = lname;
+   this.age = age;
+   this.yo = () => `Yo! I'm ${this.fname}!`; // `this` reffers to Person.
+}
