a788171fd1e83a7680ef3704485a747c6e6f6ffe
hemanth.hm <hemanth.hm@gmail.com>
2015-03-19 11:09:24 +0530

    More simpler example for lexical scope.
    Fixes #6

--- arrow.js
+++ arrow.js
@@ -1,10 +1,10 @@
-fact = (n) => { return n === 0 ? 1 : n * fact(n-1); } 
+let obj = {
+  method: function () {
+    return () => this;
+  }
+};
+// Due to lexical scope obj.method()() <===> obj
 
-fib = (n) => { return n < 2 ? n : fib(n-1) + fib(n-2); }
+let fact = (n) => { return n === 0 ? 1 : n * fact(n-1); };
 
-let Person = (fname, lname, age) => {
-   this.fname= fname;
-   this.lname = lname;
-   this.age = age;
-   this.yo = () => `Yo! I'm ${this.fname}!`; // `this` reffers to Person.
-}
+let fib = (n) => { return n < 2 ? n : fib(n-1) + fib(n-2); };
