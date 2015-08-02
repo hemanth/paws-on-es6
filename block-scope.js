9ea69e492ceb4c8c38008e9096013ae6551d59c1
hemanth.hm <hemanth.hm@gmail.com>
2015-08-02 10:58:43 +0530

    Fixed typo.
    ^

--- block-scope.js
+++ block-scope.js
@@ -3,7 +3,7 @@ var aboutme = () => {
     var investment = 1;
     const salary = 10;
   }
-  console.log(investment, slaray); // 1, salary is not defined.
+  console.log(investment, salary); // 1, salary is not defined.
 }
 
 for (let i = 0; i<10; i++) {}
