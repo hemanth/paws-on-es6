be5cc24cd9c6cf7bab988a311ef0d283028b1d69
hemanth.hm <hemanth.hm@gmail.com>
2015-08-03 23:20:26 +0530

    Multiline example.
    ^

--- template-literals.js
+++ template-literals.js
@@ -1,3 +1,10 @@
-var First = "Hemanth";
-var Last = " HM";
+let First = "Hemanth";
+let Last = " HM";
 `${ First } + ${ Last } = ${ First + Last}`  // "Hemanth + HM = Hemanth HM"
+
+
+let multiLine = `<html>
+                  <body>
+                  <p> Look Ma! Multi line! </p>
+                  </body>
+                  </html>`
