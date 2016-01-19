b192b4b865b8ef2ce6bb71a057605dcfc1662f2f
404pnf <404pnf@exmaple.org>
2016-01-19 14:02:35 +0800

    keep wm.delete, add new examples

--- weakmaps.js
+++ weakmaps.js
@@ -6,12 +6,20 @@ wm.set('life', 'life'.length) //TypeError: Invalid value used as weak map key
 
 var wmk = {};
 
-wm.set(wmk,'life');
+wm.set(wmk, 'life');
 
 wm.get(wmk); // "life"
 
 wm.has(wmk); // true
 
-wmk = undefined;
+wm.delete(wmk); // true
 
 wm.has(wmk); //false
+
+var key = {}
+
+wm.set(key, 'value');
+
+key = undefined; // key disappeared
+
+wm.has(key); // false; entry disappeared automatically
