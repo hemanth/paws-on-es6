0b0c50b0d40cc4f2d8d68fb8edebb112b4db7251
Tinh Ngo | RealCampaign <tinh.ngo@realc.int>
2016-08-19 11:26:41 +0700

    more sample

--- destructuring.js
+++ destructuring.js
@@ -1,1 +1,11 @@
 var {foo, bar} = {foo: "lorem", bar: "ipsum"};
+
+
+function foo() {
+    return [1, 2, 3];
+}
+let arr = foo(); // [1,2,3]
+
+let [a, b, c] = foo();
+console.log(a, b, c); // 1 2 3
+
